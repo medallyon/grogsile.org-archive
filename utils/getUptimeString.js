f61f5fca8b55b69d02631470661808402317e231
Tilman Raendchen <chopper98@hotmail.de>
2017-04-08 18:58:02 +0200

    don't add 's's to units if they're exactly 1

--- utils/getUptimeString.js
+++ utils/getUptimeString.js
@@ -1,26 +1,26 @@
 function getUptimeString(uptime)
 {
     let months = (uptime / 30.436875 / 24 / 60 / 60 / 1000).toFixed()
-    , days = (uptime / 24 / 60 / 60 / 1000).toFixed()
-    , hours = (uptime / 60 / 60 / 1000).toFixed()
-    , minutes = (uptime / 60 / 1000).toFixed()
-    , seconds = (uptime / 1000).toFixed()
+    , days = ((uptime / 24 / 60 / 60 / 1000) % 30.436875).toFixed()
+    , hours = ((uptime / 60 / 60 / 1000) % 24).toFixed()
+    , minutes = ((uptime / 60 / 1000) % 60).toFixed()
+    , seconds = ((uptime / 1000) % 60).toFixed()
     , output = "";
 
     if (months >= 1) {
-        output = `${months} Months, ${days} Days, ${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
+        output = `${months} Month${months === 1 ? "" : "s"}, ${days} Day${days === 1 ? "" : "s"}, ${hours} Hour${hours === 1 ? "" : "s"}, ${minutes} Minute${minutes === 1 ? "" : "s"} and ${seconds} Second${seconds === 1 ? "" : "s"}.`;
     } else
     if (days >= 1) {
-        output = `${days} Days, ${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
+        output = `${days} Day${days === 1 ? "" : "s"}, ${hours} Hour${hours === 1 ? "" : "s"}, ${minutes} Minute${minutes === 1 ? "" : "s"} and ${seconds} Second${seconds === 1 ? "" : "s"}.`;
     } else
     if (hours >= 1) {
-        output = `${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
+        output = `${hours} Hour${hours === 1 ? "" : "s"}, ${minutes} Minute${minutes === 1 ? "" : "s"} and ${seconds} Second${seconds === 1 ? "" : "s"}.`;
     } else
     if (minutes >= 1) {
-        output = `${minutes} Minutes and ${seconds} Seconds.`;
+        output = `${minutes} Minute${minutes === 1 ? "" : "s"} and ${seconds} Second${seconds === 1 ? "" : "s"}.`;
     } else
     if (seconds >= 1) {
-        output = `${seconds} Seconds.`;
+        output = `${seconds} Second${seconds === 1 ? "" : "s"}.`;
     }
 
     return output;
