95e5298eb2c9b529be891891e4d2b80f50719f3a
Tilman Raendchen <chopper98@hotmail.de>
2017-04-30 00:55:25 +0100

    fix non-account users having access to website

--- web/middleware/isAccountSetup/isAccountSetup.js
+++ web/middleware/isAccountSetup/isAccountSetup.js
@@ -3,11 +3,11 @@ const fs = require("fs-extra")
 
 function isAccountSetup(req, res, next)
 {
-    fs.readdir(join(__data, "users", req.user.id), (err, files) => {
-        if (err) console.error(err);
+    fs.readJson(join(__data, "users", req.user.id, "account.json"), (err, account) => {
+        if (err) return res.send(err);
 
-        if (files.indexOf("account.json") > -1) next();
-        else res.redirect("/account");
+        if (account.accountName === "undefined") res.redirect("/account");
+        else next();
     });
 }
 
