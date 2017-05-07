8d97c60e3a0fe5b59c3c9254022252e853802f9c
Tilman Raendchen <chopper98@hotmail.de>
2017-05-07 03:42:59 +0100

    performance fixes

--- web/domains/esoi.grogsile.me/index.js
+++ web/domains/esoi.grogsile.me/index.js
@@ -7,8 +7,8 @@ app = express();
 // 
 app.set("views", join(__webdir, "views", "esoi"));
 
-app.use( bodyparser.json() );
-app.use( bodyparser.urlencoded({ extended: true }) );
+app.use( bodyparser.json({ limit: "10mb" }) );
+app.use( bodyparser.urlencoded({ limit: "10mb", extended: true }) );
 app.use(require(join(__webdir, "routers", "esoi.grogsile.js", "esoi.grogsile.js")));
 /////////////////////////////////////////////////////////////
 
