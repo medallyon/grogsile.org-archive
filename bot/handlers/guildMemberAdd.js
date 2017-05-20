6b6c648612894ca67c18050a0704f9746031b308
Tilman Raendchen <chopper98@hotmail.de>
2017-05-20 22:03:38 +0100

    add appropriate roles to members on ESOI on join

--- bot/handlers/guildMemberAdd.js
+++ bot/handlers/guildMemberAdd.js
@@ -4,6 +4,7 @@ const Discord = require("discord.js")
 
 dClient.on("guildMemberAdd", function(member)
 {
+    // process welcome message
     utils.readGuildConfig(member.guild).then(function(config)
     {
         if (config.guild.welcomeMessage.enabled)
@@ -12,13 +13,16 @@ dClient.on("guildMemberAdd", function(member)
                 if (err) console.error(err);
 
                 // do not allow duplicate name entries
-                if (savedVars.latestMembers.indexOf(member.id) === -1) savedVars.latestMembers.push(member.id);
+                if (savedVars.latestMembers.map(x => x.id).indexOf(member.id) === -1) savedVars.latestMembers.push({
+                    id: member.id,
+                    username: member.user.username
+                });
 
                 // make sure that members do not exceed limit of 5 per welcome
                 config.guild.welcomeMessage.maxMembers = config.guild.welcomeMessage.maxMembers <= 5 ? config.guild.welcomeMessage.maxMembers : 5;
                 if (savedVars.latestMembers.length === config.guild.welcomeMessage.maxMembers)
                 {
-                    dClient.channels.get(config.guild.welcomeMessage.channel).sendMessage(utils.processWelcomeMessage(config.guild.welcomeMessage.message, member.guild, savedVars.latestMembers));
+                    dClient.channels.get(config.guild.welcomeMessage.channel).send(utils.processWelcomeMessage(config.guild.welcomeMessage.message, member.guild, savedVars.latestMembers));
 
                     savedVars.latestMembers = [];
                 }
@@ -27,4 +31,15 @@ dClient.on("guildMemberAdd", function(member)
             });
         }
     }).catch(console.error);
+
+    // add roles to member if existing
+    fs.access(join(__data, "users", member.id, "account.json"), (err) => {
+        if (err) return;
+
+        fs.readJson(join(__data, "users", member.id, "account.json"), (err, account) => {
+            if (err) console.error(err);
+
+            modules.addToEsoRank(member.id, account.server, account.platform, account.alliance);
+        });
+    });
 });
