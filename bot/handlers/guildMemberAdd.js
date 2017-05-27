d50ff06c4e493ecbc97dd91462539df6210a16e8
Tilman Raendchen <chopper98@hotmail.de>
2017-05-27 16:32:27 +0100

    enable option to send Welcome to member's DM

--- bot/handlers/guildMemberAdd.js
+++ bot/handlers/guildMemberAdd.js
@@ -9,26 +9,37 @@ dClient.on("guildMemberAdd", function(member)
     {
         if (config.guild.welcomeMessage.enabled)
         {
-            fs.readJson(join(__data, "guilds", member.guild.id, "welcomeMessage", "savedVariables.json"), (err, savedVars) => {
-                if (err) console.error(err);
+            // send private welcome message
+            if (config.guild.welcomeMessage.direct.enabled)
+            {
+                let details = [{ id: member.id, username: member.user.username }];
+                member.send(utils.processWelcomeMessage(config.guild.welcomeMessage.direct.message, member.guild, details)).catch(console.error);
+            }
 
-                // do not allow duplicate name entries
-                if (savedVars.latestMembers.map(x => x.id).indexOf(member.id) === -1) savedVars.latestMembers.push({
-                    id: member.id,
-                    username: member.user.username
+            // send guild-wide welcome message
+            if (!(config.guild.welcomeMessage.direct.enabled && config.guild.welcomeMessage.direct.disableGuildWelcome))
+            {
+                fs.readJson(join(__data, "guilds", member.guild.id, "welcomeMessage", "savedVariables.json"), (err, savedVars) => {
+                    if (err) console.error(err);
+
+                    // do not allow duplicate name entries
+                    if (savedVars.latestMembers.map(x => x.id).indexOf(member.id) === -1) savedVars.latestMembers.push({
+                        id: member.id,
+                        username: member.user.username
+                    });
+
+                    // make sure that members do not exceed limit of 5 per welcome
+                    config.guild.welcomeMessage.maxMembers = config.guild.welcomeMessage.maxMembers <= 5 ? config.guild.welcomeMessage.maxMembers : 5;
+                    if (savedVars.latestMembers.length === config.guild.welcomeMessage.maxMembers)
+                    {
+                        dClient.channels.get(config.guild.welcomeMessage.channel).send(utils.processWelcomeMessage(config.guild.welcomeMessage.message, member.guild, savedVars.latestMembers)).catch(console.error);
+
+                        savedVars.latestMembers = [];
+                    }
+
+                    fs.outputJson(join(__data, "guilds", member.guild.id, "welcomeMessage", "savedVariables.json"), savedVars, (err) => { if (err) console.error(err) });
                 });
-
-                // make sure that members do not exceed limit of 5 per welcome
-                config.guild.welcomeMessage.maxMembers = config.guild.welcomeMessage.maxMembers <= 5 ? config.guild.welcomeMessage.maxMembers : 5;
-                if (savedVars.latestMembers.length === config.guild.welcomeMessage.maxMembers)
-                {
-                    dClient.channels.get(config.guild.welcomeMessage.channel).send(utils.processWelcomeMessage(config.guild.welcomeMessage.message, member.guild, savedVars.latestMembers));
-
-                    savedVars.latestMembers = [];
-                }
-
-                fs.outputJson(join(__data, "guilds", member.guild.id, "welcomeMessage", "savedVariables.json"), savedVars, (err) => { if (err) console.error(err) });
-            });
+            }
         }
     }).catch(console.error);
 
