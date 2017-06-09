b7cbb70b8197018af8f404e6068d1bcc88fde48a
Tilman Raendchen <chopper98@hotmail.de>
2017-06-10 00:33:45 +0100

    create initial module

---
+++ bot/modules/esoItem/index.js
@@ -0,0 +1,41 @@
+const request = require("request")
+, cheerio = require("cheerio");
+
+const baseSearchString = "http://esolog.uesp.net/viewlog.php?searchtype=minedItemSummary&search="
+, baseImgURL = "http://esoitem.uesp.net/item-"
+, baseImgModifiers = "-10-2.png";
+
+function summarizeItem($item)
+{
+    return {
+        name: $item.children().eq(2).text(),
+        url: `${baseImgURL}${$item.children().eq(4).text()}${baseImgModifiers}`
+    }
+}
+
+function searchUESPItem(item)
+{
+    return new Promise(function(resolve, reject)
+    {
+        request(`${baseSearchString}${item}`, (err, res, body) => {
+            if (err) return reject(err);
+
+            let $ = cheerio.load(body);
+
+            if ($("table").children().length <= 1) return reject(new Error(`No items found matching ${item}`));
+
+            let $minedItem = $("table").children().eq(1);
+            resolve(summarizeItem($minedItem));
+        });
+    });
+}
+
+function esoItem(msg, item)
+{
+    searchUESPItem(item)
+    .then(itemObj => {
+        msg.channel.send({ files: [{ "name": `${itemObj.name}.png`, "attachment": itemObj.url }] }).catch(console.error);
+    }).catch(console.error);
+}
+
+module.exports = esoItem;
