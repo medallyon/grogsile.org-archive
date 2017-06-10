3c00d8c6fe35551313832b23773c846a6249ef40
Tilman Raendchen <chopper98@hotmail.de>
2017-06-10 21:57:47 +0100

    fix #22
    more constants will be introduced as needed

---
+++ constants/constants.js
@@ -0,0 +1,8 @@
+// === [ LIBRARIES ] === //
+global.Discord = require("discord.js");
+global.fs = require("fs-extra");
+global.path = require("path");
+global.join = path.join;
+
+// === [ EMBEDS ] === //
+constants.discord.embed.color = utils.randColor();
