051861a59823159c3b177d4273311721175055b4
Tilman Raendchen <chopper98@hotmail.de>
2017-06-17 15:16:24 +0100

    If `eval` is `undefined`, send `undefined`

--- bot/modules/evaluate/index.js
+++ bot/modules/evaluate/index.js
@@ -2,7 +2,7 @@ function evaluate(msg)
 {
     try {
         let evaluated = eval(msg.args.join(" "));
-        msg.channel.send(evaluated, { code: "js" });
+        msg.channel.send((evaluated !== undefined ? evaluated : "undefined"), { code: "js" });
     } catch (err) {
         msg.channel.send(err, { code: "js" });
     }
