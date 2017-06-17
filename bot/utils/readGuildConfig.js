edc21e301db56b14ec33ed864e4e295468f53291
Tilman Raendchen <chopper98@hotmail.de>
2017-06-17 15:19:32 +0100

    `guild` exists implicitly. No need for this line

--- bot/utils/readGuildConfig.js
+++ bot/utils/readGuildConfig.js
@@ -2,8 +2,6 @@ function readGuildConfig(guild)
 {
     return new Promise(function(resolve, reject)
     {
-        if (!guild) reject(new ReferenceError("Guild must be an object"));
-
         fs.readJson(join(__data, "guilds", guild.id, "config.json"), (err, config) => {
             if (err) reject(err);
             else resolve(config);
