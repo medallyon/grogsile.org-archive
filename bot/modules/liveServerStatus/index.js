dcbc98fbcf10e3d3f8d257677de25be53f25727a
Tilman Raendchen <chopper98@hotmail.de>
2017-07-13 15:48:47 +0100

    fix #52 #46

--- bot/modules/liveServerStatus/index.js
+++ bot/modules/liveServerStatus/index.js
@@ -4,9 +4,9 @@ const LIVE_CHANNEL = "319641305388941315"
 function structureEmbed(status)
 {
     let e = new Discord.RichEmbed()
-        .setAuthor("ESO Server Status", dClient.user.displayAvatarURL, STATUS_DOMAIN)
+        .setAuthor("ESO Server Status", "", STATUS_DOMAIN)
         .setDescription("This live panel is updated every 5 minutes to check for all ESO Server Statuses.")
-        .setFooter(`Brought to you by Grogsile, Inc. | ${utils.fancyESODate(new Date())} ${new Date().getUTCHours()}:${new Date().getUTCMinutes()} UTC`);
+        .setFooter(`Brought to you by Grogsile, Inc. | ${utils.fancyESODate(new Date())} ${new Date().getUTCHours()}:${new Date().getUTCMinutes()} UTC`, dClient.user.displayAvatarURL);
 
     e.addField("\u200b", "**[PC]**");
     for (let server in status["PC"])
@@ -32,9 +32,9 @@ function structureEmbed(status)
 function prepareAnnouncement(servers)
 {
     let e = new Discord.RichEmbed()
-        .setAuthor("ESO Server Status", dClient.user.displayAvatarURL, STATUS_DOMAIN)
+        .setAuthor("ESO Server Status", "", STATUS_DOMAIN)
         .setDescription("The following servers have been updated:")
-        .setFooter(`Brought to you by Grogsile, Inc. | ${utils.fancyESODate(new Date())} ${new Date().getUTCHours()}:${new Date().getUTCMinutes()} UTC`)
+        .setFooter(`Brought to you by Grogsile, Inc. | ${utils.fancyESODate(new Date())} ${new Date().getUTCHours()}:${new Date().getUTCMinutes()} UTC`, dClient.user.displayAvatarURL)
     , strings = [];
 
     if (servers.findIndex(x => x.platform === "PC") > -1)
@@ -69,23 +69,54 @@ function prepareAnnouncement(servers)
     return e;
 }
 
-function toggleRole(role)
+function toggleRoles(doToggle, roles)
 {
-    return role.setMentionable(!role.mentionable);
+    return new Promise(function(resolve, reject)
+    {
+        if (!doToggle) return resolve(roles);
+        if (!roles.length) return resolve(roles);
+
+        for (let i = 0; i < roles.length; i++)
+        {
+            let role = roles[i];
+
+            role.setMentionable(!role.mentionable)
+            .then(newRole => {
+                if (i === roles.length - 1)
+                {
+                    resolve(roles);
+                }
+            }).catch(err => {
+                if (i === roles.length - 1)
+                {
+                    resolve(roles);
+                }
+            });
+        }
+    });
+}
+
+function deletePreviousUpdate(doDelete, channel, id)
+{
+    return new Promise(function(resolve, reject)
+    {
+        if (!doDelete) return resolve(null);
+
+        channel.fetchMessage(id)
+        .then(msg => {
+            resolve(msg.delete());
+        }).catch(resolve);
+    });
 }
 
 function liveServerStatus()
 {
-    let statusChannel = dClient.guilds.get(constants.discord.esoi.id).channels.get(LIVE_CHANNEL);
-
-    fs.readJson(join(__dirname, "savedVars.json"), function(err, savedVars)
+    utils.fetchServerStatus().then(function(status)
     {
-        if (err) return console.error(err);
-
-        let statusMessageId = savedVars.message;
-
-        utils.fetchServerStatus().then(function(status)
+        fs.readJson(join(__dirname, "savedVars.json"), function(err, savedVars)
         {
+            if (err) return console.error(err);
+
             let changedServers = [];
             for (let platform in status)
             {
@@ -97,51 +128,78 @@ function liveServerStatus()
                     }
                 }
             }
+
             savedVars.status = status;
-
-            const finalEmbed = structureEmbed(status);
-            statusChannel.fetchMessage(statusMessageId)
-            .then(liveMessage => {
-                statusChannel.fetchMessage(statusMessageId).then(function(msg)
-                {
-                    msg.edit({ embed: finalEmbed });
-
-                    fs.outputJson(join(__dirname, "savedVars.json"), savedVars);
-                }).catch(console.error);
-            })
-            .catch(err => {
-                statusChannel.send({ embed: finalEmbed }).then(message => {
-                    savedVars.message = message.id;
-
-                    fs.outputJson(join(__dirname, "savedVars.json"), savedVars);
-                });
-            });
-
-            if (changedServers.length)
+            fs.outputJson(join(__dirname, "savedVars.json"), savedVars, err => { if (err) console.error(err) });
+            
+            const finalPanelEmbed = structureEmbed(status)
+            , finalUpdateEmbed = prepareAnnouncement(changedServers);
+            
+            for (let guild of dClient.guilds.values())
             {
-                let roleToMention = statusChannel.guild.roles.get(constants.discord.esoi.roles.ServerUpdates);
+                if (!guild.available) continue;
 
-                if (!roleToMention.mentionable)
+                utils.readGuildConfig(guild).then(function(config)
                 {
-                    toggleRole(roleToMention)
-                    .then(role => {
-                        statusChannel.guild.channels.find("name", "announcements").send(roleToMention.toString(), { embed: prepareAnnouncement(changedServers) }).catch(console.error)
-                        .then(msg => {
-                            toggleRole(role).catch(console.error);
+                    if (err) return console.error(err);
+                    const baseGuildPath = join(__data, "guilds", guild.id);
+
+                    if (!config.guild.liveServerStatus.panel.enabled || !config.guild.liveServerStatus.update.enabled) return;
+                    
+                    else
+                    {
+                        fs.readJson(join(baseGuildPath, "liveServerUpdate", "savedVariables.json"), function(err, liveVars)
+                        {
+                            if (err) return console.error(err);
+
+                            // live panel
+                            if (config.guild.liveServerStatus.panel.enabled)
+                            {
+                                let liveChannel = guild.channels.get(config.guild.liveServerStatus.panel.channel);
+
+                                liveChannel.fetchMessage(liveVars.panelId)
+                                .then(panelMessage => {
+                                    panelMessage.edit({ embed: finalPanelEmbed }).catch(console.error);
+                                }).catch(err => {
+                                    liveChannel.send({ embed: finalPanelEmbed })
+                                    .then(function(sentMessage)
+                                    {
+                                        liveVars.panelId = sentMessage.id;
+                                        fs.outputJson(join(baseGuildPath, "liveServerUpdate", "savedVariables.json"), liveVars, err => { if (err) console.error(err) });
+                                    }).catch(console.error);
+                                });
+                            }
+
+                            // global announcements
+                            if (config.guild.liveServerStatus.update.enabled)
+                            {
+                                if (changedServers.length)
+                                {
+                                    let updateChannel = guild.channels.get(config.guild.liveServerStatus.update.channel)
+                                    , roles = config.guild.liveServerStatus.update.roles.map(x => guild.roles.get(x));
+
+                                    toggleRoles(config.guild.liveServerStatus.update.toggleRoles, roles.filter(r => !r.mentionable))
+                                    .then(function(modifiedRoles)
+                                    {
+                                        deletePreviousUpdate(config.guild.liveServerStatus.update.deletePrevious, updateChannel, liveVars.updateId)
+                                        .then(function(deletedMessage)
+                                        {
+                                            updateChannel.send(roles.map(r => r.toString()).join(" "), { embed: finalUpdateEmbed })
+                                            .then(sentMessage => {
+                                                toggleRoles(config.guild.liveServerStatus.update.toggleRoles, roles);
+                                                liveVars.updateId = sentMessage.id;
+                                                fs.outputJson(join(baseGuildPath, "liveServerUpdate", "savedVariables.json"), liveVars, (err) => { if (err) console.error(err) });
+                                            }).catch(console.error);
+                                        });
+                                    }).catch(console.error);
+                                }
+                            }
                         });
-                    }).catch(console.error);
-                }
-
-                else
-                {
-                    statusChannel.guild.channels.find("name", "announcements").send(roleToMention.toString(), { embed: prepareAnnouncement(changedServers) }).catch(console.error)
-                    .then(msg => {
-                        toggleRole(roleToMention).catch(console.error);
-                    });
-                }
+                    }
+                }).catch(console.error);
             }
-        }).catch(console.error);
-    });
+        });
+    }).catch(console.error);
 }
 
 module.exports = liveServerStatus;
