95722b164e585c1c8cb2c1458f7b879a9c5d6c96
Tilman Raendchen <chopper98@hotmail.de>
2017-07-13 20:52:34 +0100

    fix minor bug with deprecated perm strings

--- bot/utils/determinePermissions.js
+++ bot/utils/determinePermissions.js
@@ -13,7 +13,7 @@ var determinePermissions = function(member)
         } else if (member.guild.owner.id === member.id) {
             return 400;
         // else if member is an administrator
-        } else if (member.hasPermission("ADMINISTRATOR") || member.hasPermission(["MANAGE_CHANNELS", "MANAGE_MESSAGES", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_ROLES_OR_PERMISSIONS"])) {
+        } else if (member.hasPermission("ADMINISTRATOR") || member.hasPermission(["MANAGE_CHANNELS", "MANAGE_MESSAGES", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_ROLES"])) {
             return 300;
         // else if member is a moderator
         } else if (member.hasPermission(["MANAGE_MESSAGES", "MUTE_MEMBERS", "KICK_MEMBERS"])) {
