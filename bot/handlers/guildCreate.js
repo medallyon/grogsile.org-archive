d9da9c25123a62d2f782a66d668f640d4e8aefa9
Tilman Raendchen <chopper98@hotmail.de>
2017-07-14 10:48:27 +0100

    clean up handler - readability

--- bot/handlers/guildCreate.js
+++ bot/handlers/guildCreate.js
@@ -1,13 +1,20 @@
 dClient.on("guildCreate", (guild) => {
-    let guildConfig = _templates.guild;
-    
-    guildConfig.id = guild.id;
-
-    let newsChannel = (guild.channels.exists("name", "announcements")) ? guild.channels.find("name", "announcements") : guild.defaultChannel;
-    guildConfig.eso.news.channel = newsChannel.id;
-    guildConfig.eso.youtube.channel = newsChannel.id;
-
-    fs.outputJson(join(__data, "guilds", guild.id, "config.json"), guildConfig, (err) => { if (err) console.error(err) });
-    fs.outputJson(join(__data, "guilds", guild.id, "welcomeMessage", "savedVariables.json"), { latestMembers: [] }, (err) => { if (err) console.error(err) });
-    fs.outputJson(join(__data, "guilds", guild.id, "RSS", "savedVariables.json"), { latest: 0 }, (err) => { if (err) console.error(err) });
+    curateConfig(_templates.guild, guild);
+    prepareFiles(guild);
 });
+
+function curateConfig(config, guild)
+{
+    config.id = guild.id;
+
+    fs.outputJson(join(__data, "guilds", guild.id, "config.json"), config, (err) => { if (err) console.error(err) });
+}
+
+function prepareFiles(guild)
+{
+    const baseGuildPath = join(__data, "guilds", guild.id);
+
+    fs.outputJson(join(baseGuildPath, "welcomeMessage", "savedVariables.json"), { latestMembers: [] }, (err) => { if (err) console.error(err) });
+    fs.outputJson(join(baseGuildPath, "RSS", "savedVariables.json"), { latest: 0 }, (err) => { if (err) console.error(err) });
+    fs.outputJson(join(baseGuildPath, "liveServerUpdate", "savedVariables.json"), { panelId: "0", updateId: "0" }, (err) => { if (err) console.error(err) });
+}
