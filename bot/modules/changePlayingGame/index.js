3b61543643840c06ab08553f94ba70e9434be5c6
Tilman Raendchen <chopper98@hotmail.de>
2017-07-14 11:50:39 +0100

    rename setGameInterval to changePlayingGame

---
+++ bot/modules/changePlayingGame/index.js
@@ -0,0 +1,30 @@
+function applyGame(recentGames, gameArray)
+{
+    let game = gameArray.filter(g => recentGames.some(x => x !== g))[Math.floor(Math.random() * gameArray.length)];
+    recentGames.push(game);
+
+    if (recentGames.length > 2) recentGames.shift();
+
+    return dClient.user.setGame(game);
+}
+
+function setGameInterval(gameArray = null)
+{
+    let recentGames = [];
+    if (!gameArray)
+    {
+        fs.readJson(join(__dirname, "games.json"), function(err, games)
+        {
+            if (err) return console.error(err);
+
+            applyGame(recentGames, games).catch(console.error);
+        });
+    }
+
+    else
+    {
+        applyGame(recentGames, gameArray).catch(console.error);
+    }
+}
+
+module.exports = setGameInterval;
