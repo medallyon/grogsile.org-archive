82115f60f340d1c2f14deafc3712fb0b6c0aab5e
Tilman Raendchen <chopper98@hotmail.de>
2017-07-14 10:53:22 +0100

    rework module, fix #36

--- bot/modules/esoItem/index.js
+++ bot/modules/esoItem/index.js
@@ -1,15 +1,33 @@
-const request = require("request")
-, cheerio = require("cheerio");
+const baseImgURL = "http://esoitem.uesp.net/item-"
+, baseImgModifier = "-66-5.png";
 
-const baseSearchString = "http://esolog.uesp.net/viewlog.php?searchtype=minedItemSummary&search="
-, baseImgURL = "http://esoitem.uesp.net/item-"
-, baseImgModifiers = "-66-5.png";
-
-function summarizeItem($item)
+function deduceItemSummary([item, number])
 {
-    return {
-        name: $item.children().eq(3).text(),
-        url: `${baseImgURL}${$item.children().eq(4).text()}${baseImgModifiers}`
+    return [{
+        name: item.name,
+        url: baseImgURL + item.id + baseImgModifier
+    }, number];
+}
+
+function findItem(itemCollection, item)
+{
+    // check if requested item is itemID
+    if (itemCollection.has(item)) return itemCollection.get(item);
+
+    // check if requested item is item.name
+    if (itemCollection.exists("name", item)) return itemCollection.find("name", item);
+
+    // filter out items containing requested item's words
+    let matchingItems = itemCollection.filter(x => x.name.toLowerCase().includes(item.toLowerCase()));
+    console.log(matchingItems.size);
+
+    if (matchingItems.size === 1) return matchingItems.first();
+
+    if (matchingItems.size === 0) return null;
+
+    if (matchingItems.size > 1)
+    {
+        return [matchingItems.array()[Math.floor(Math.random() * matchingItems.size)], matchingItems.size];
     }
 }
 
@@ -17,15 +35,13 @@ function searchUESPItem(item)
 {
     return new Promise(function(resolve, reject)
     {
-        request(`${baseSearchString}${item}`, (err, res, body) => {
+        fs.readJson(join(__dirname, "items.json"), (err, items) => {
             if (err) return reject(err);
 
-            let $ = cheerio.load(body);
+            let foundItem = findItem(new Discord.Collection(items), item);
 
-            if ($("table").children().length <= 1) return reject(`No items found matching '${item}'`);
-
-            let $minedItem = $("table").children().eq(1);
-            resolve(summarizeItem($minedItem));
+            if (foundItem) resolve(deduceItemSummary(foundItem));
+            else reject(`Item '${item}' not found`);
         });
     });
 }
@@ -33,8 +49,8 @@ function searchUESPItem(item)
 function esoItem(msg, item)
 {
     searchUESPItem(item)
-    .then(itemObj => {
-        msg.channel.send({ files: [{ "name": `${itemObj.name}.png`, "attachment": itemObj.url }] }).catch(console.error);
+    .then(([itemObj, otherItems]) => {
+        msg.channel.send((otherItems > 1) ? `There are **${otherItems}** other items like this one` : "", { files: [{ "name": `${itemObj.name}.png`, "attachment": itemObj.url }] }).catch(console.error);
     }).catch(console.log);
 }
 
