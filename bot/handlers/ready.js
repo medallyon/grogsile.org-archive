844dfdb7e00c7ff05cca22b789fa3f52362ddcfd
Tilman Raendchen <chopper98@hotmail.de>
2017-07-19 20:21:00 +0100

    change cronjob to run immediately

--- bot/handlers/ready.js
+++ bot/handlers/ready.js
@@ -26,5 +26,5 @@ dClient.once("ready", () => {
 
     new CronJob("0 0,5,10,15,20,25,30,35,40,45,50,55,60 * * * *", modules.liveServerStatus, null, true, "UTC");
     new CronJob("0 0 * * * *", modules.esoPatchNotes, null, true, "UTC");
-    new CronJob("0 */15 * * * *", modules.changePlayingGame, null, true, "UTC");
+    new CronJob("0 */15 * * * *", modules.changePlayingGame, null, true, "UTC", {}, true);
 });
