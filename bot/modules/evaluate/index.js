785d1a04566417ce4031d53696245caf069a33df
Tilman Raendchen <Medallyon@users.noreply.github.com>
2017-08-16 02:24:48 +0200

    add `catch` functions

--- bot/modules/evaluate/index.js
+++ bot/modules/evaluate/index.js
@@ -2,9 +2,9 @@ function evaluate(msg)
 {
     try {
         let evaluated = eval(msg.args.join(" "));
-        msg.channel.send((evaluated !== undefined ? evaluated : "undefined"), { code: "js" });
+        msg.channel.send((evaluated !== undefined ? evaluated : "undefined"), { code: "js" }).catch(console.error);
     } catch (err) {
-        msg.channel.send(err, { code: "js" });
+        msg.channel.send(err, { code: "js" }).catch(console.error);
     }
 }
 
