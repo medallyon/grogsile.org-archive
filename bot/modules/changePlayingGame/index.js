b7df65a17766e5b9136e685aaec99f8b532522fa
Tilman Raendchen <Medallyon@users.noreply.github.com>
2017-08-17 03:50:23 +0200

    rework changePlayingGame

--- bot/modules/changePlayingGame/index.js
+++ bot/modules/changePlayingGame/index.js
@@ -1,29 +1,29 @@
 let recentGames = [];
 
-function applyGame(recentGames, games)
+function chooseRandomGame(games)
 {
-    const validGames = games.filter(g => !recentGames.includes(g));
+    const validGames = games.filter(x => !recentGames.includes(x));
     const game = validGames[Math.floor(Math.random() * validGames.length)];
-    
-    recentGames.push(game);
-    if (recentGames.length > 3) recentGames.shift();
 
-    return dClient.user.setGame(game);
+    recentGames.push(game);
+    if (recentGames.length > Math.floor(games.length * 0.2)) recentGames.shift();
+
+    return game;
 }
 
-function changePlayingGame(gameArray = null)
+function changePlayingGame()
 {
-    if (!gameArray)
+    fs.readJson(join(__dirname, "games.json"), function(err, games)
     {
-        fs.readJson(join(__dirname, "games.json"), function(err, games)
-        {
-            if (err) return console.error(err);
+        if (err) return console.error(err);
 
-            applyGame(recentGames, games).catch(console.error);
-        });
-    }
+        if (!Array.isArray(games) || !games.length) return console.error(new Error("Cannot apply new game since games.json file is empty or not an Array."));
 
-    else applyGame(recentGames, gameArray).catch(console.error);
+        const game = chooseRandomGame(games);
+        if (!game) return console.error(new Error(`'game' is not a game: ${game}`));
+
+        dClient.user.setGame(game).catch(console.error);
+    });
 }
 
 module.exports = changePlayingGame;
