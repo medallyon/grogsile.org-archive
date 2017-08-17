dd5a779c2388baa1663003721dd807b76b870769
Tilman Raendchen <Medallyon@users.noreply.github.com>
2017-08-17 03:50:33 +0200

    fix avatarURL param

--- bot/modules/about/index.js
+++ bot/modules/about/index.js
@@ -1,7 +1,7 @@
 function createEmbed()
 {
     return new Discord.MessageEmbed(utils.createEmptyRichEmbedObject())
-        .setAuthor(dClient.user.username, dClient.user.avatarURL, "https://esoi.grogsile.me/")
+        .setAuthor(dClient.user.username, dClient.user.displayAvatarURL(), "https://esoi.grogsile.me/")
         .setDescription(`The Tamriel Messenger is a Discord Bot catered to fans of the Elder Scrolls series, especially TES: Online. With this bot, you will be able to easily integrate your whole ESO experience within Discord. See \`${dClient.config.discord.prefix}help\` for further assistance.`)
         .addField("Website", "[Grogsile Inc.](https://esoi.grogsile.me/)", true)
         .addField("Creator", "@Medallyon#5012", true)
