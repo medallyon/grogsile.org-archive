4378f810a2d58bafe80c922b2a25b6513e3ae57b
Tilman Raendchen <chopper98@hotmail.de>
2017-10-25 02:36:38 +0100

    make loading of images dynamic on homepage

---
+++ web/.pub_src/esoi/js/cover/loadHighRes.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{
+    /*$(".masthead-brand").waitForImages(function()
+    {
+        $(this).css("background-image", `url("https://i.grogsile.org/esoi/img/cover/brand.png")`);
+    });*/
+
+    /*$(".cover-img-hirez").waitForImages(function()
+    {
+        $(this).parent().css("background-image", "");
+    })*/
+});
