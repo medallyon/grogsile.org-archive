6dbe8c57d0a8aa5541997024d31985a53a7c1715
Tilman Raendchen <chopper98@hotmail.de>
2018-01-11 16:51:56 +0000

    general quality improvements, fix some logic errors

--- utils/treatConfig.js
+++ utils/treatConfig.js
@@ -1,9 +1,20 @@
 // Discord.Guild : guild, Object : object
 function treatConfig(guild, config)
 {
-    config.raw = JSON.parse(JSON.stringify(config));
     config._guild = guild;
     config._path = join(__data, "guilds", guild.id, "config.json");
+
+    Object.defineProperty(config, "raw", { get: function()
+    {
+        let shallowCopy = Object.assign({}, config);
+
+        delete shallowCopy._guild;
+        delete shallowCopy._path;
+        delete shallowCopy._save;
+        delete shallowCopy._reload;
+
+        return shallowCopy;
+    } });
 
     // Object : data, Boolean : merge
     config._save = function(data = null, merge = true)
@@ -13,12 +24,13 @@ function treatConfig(guild, config)
             _guild = config._guild;
             _path = config._path;
             _save = config._save;
+            _reload = config._reload;
             
             if (data !== null)
             {
                 if (merge)
                 {
-                    Object.assign(config, data);
+                    config = Object.assign(config, data);
                     data = config;
                 } else config = data;
             } else data = config;
@@ -26,14 +38,22 @@ function treatConfig(guild, config)
             if (data._guild) delete data._guild;
             if (data._path) delete data._path;
             if (data._save) delete data._save;
+            if (data._reload) delete data._reload;
+            if (data.raw) delete data.raw;
 
             fs.outputJson(_path, data, { spaces: 2 }).then(function()
             {
+                config = data;
+                config.raw = JSON.parse(JSON.stringify(data));
+
+                _guild.config = config;
+                
                 config._guild = _guild;
                 config._path = _path;
                 config._save = _save;
+                config._reload = _reload;
 
-                resolve(config);
+                resolve();
             }).catch(reject);
         });
     }
