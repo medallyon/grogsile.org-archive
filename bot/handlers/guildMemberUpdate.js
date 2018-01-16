5570441170c26d679c79b39de1e465434c81ff27
Tilman Raendchen <chopper98@hotmail.de>
2018-01-16 23:52:12 +0000

    add functionality to update nicknames on ESOI

---
+++ bot/handlers/guildMemberUpdate.js
@@ -0,0 +1,34 @@
+function updateMemberNickname(member)
+{
+    fs.readJson(join(__data, "users", member.id, "account.json")).then(function(account)
+    {
+        let nickname = member.displayName, prefix;
+        if (account.nickname)
+        {
+            let newPrefix = `[${account.platform}] `;
+            if (account.platform === "XBOne") newPrefix = `[XB1] `;
+
+            if (/\[(?:PC|XB1|PS4)\] /g.test(nickname))
+            {
+                prefix = /(\[(?:PC|XB1|PS4)\] )/g.exec(nickname)[1];
+                nickname = nickname.replace(prefix, newPrefix);
+            } else nickname = newPrefix + nickname;
+        } else
+
+        if (!account.nickname)
+        {
+            if (/\[(?:PC|XB1|PS4)\] /g.test(nickname))
+            {
+                prefix = /(\[(?:PC|XB1|PS4)\] )/g.exec(nickname)[1];
+                nickname = nickname.replace(prefix, "");
+            }
+        }
+
+        member.setNickname(nickname, "Web Account Nickname Update").catch(console.error);
+    }).catch(console.error);
+}
+
+dClient.on("guildMemberUpdate", function(oldMember, newMember)
+{
+    if (_esoi.guild.members.has(newMember.id) && oldMember.displayName !== newMember.displayName) updateMemberNickname(newMember);
+});
