f01df2d9d7353d67e45bdfed6874d31a82df38b8
Tilman Raendchen <chopper98@hotmail.de>
2018-02-17 06:18:31 +0000

    fix #62 and apply to 'esoItem' module

--- bot/modules/esoItem/index.js
+++ bot/modules/esoItem/index.js
@@ -1,71 +1,92 @@
 const jsonStream = require("JSONStream");
 
-const baseImgURL = "http://esoitem.uesp.net/item-"
-, baseImgModifier = "-66-5.png";
+const baseImgURL = "http://esolog.uesp.net/itemLinkImage.php?version=17pts&level=66&quality=5&itemid="
+, baseSummaryURL = "http://esolog.uesp.net/itemLink.php?version=17pts&summary&itemid=";
 
 database = [];
 fs.createReadStream(join(__dirname, "items.json"))
     .pipe(jsonStream.parse("*"))
     .on("error", console.error)
-    .on("end", () => {
-        database.map(item => { item[1].name = item[1].name.toLowerCase() });
-        dClient.eso.items = new Discord.Collection(database);
-        database = null; 
-    })
     .on("data", function(item)
     {
         database.push(item);
+    })
+    .on("end", () => {
+        dClient.eso.items = new Discord.Collection(database);
+        database = null;
     });
 
-function deduceItemSummary([item, number])
+function deduceItemSummary(item)
 {
-    return [{
-        name: item.name,
-        url: baseImgURL + item.id + baseImgModifier
-    }, number];
+    return Object.assign({
+        url: baseImgURL + item.id
+    }, item);
 }
 
-function findItem(itemCollection, item)
+function findItems(itemCollection, item)
 {
     // check if requested item is itemID
-    if (itemCollection.has(item)) return [itemCollection.get(item), 0];
+    if (itemCollection.has(item)) return [itemCollection.get(item)];
 
-    // check if requested item is item.name
-    if (itemCollection.exists("name", item)) return [itemCollection.find("name", item), 0];
-
-    // filter out items containing requested item's words
-    let matchingItems = itemCollection.filter(x => x.name.toLowerCase().includes(item.toLowerCase()));
-
-    if (matchingItems.size === 1) return [matchingItems.first(), 0];
-
-    if (matchingItems.size === 0) return [null, 0];
-
-    if (matchingItems.size > 1) return [matchingItems.array()[Math.floor(Math.random() * matchingItems.size)], matchingItems.size];
+    // otherwise, return all items containing requested item's words
+    return itemCollection.filter(x => x.name.toLowerCase().includes(item.toLowerCase())).array();
 }
 
-function searchUESPItem(item)
+function searchUESPItems(item)
 {
-    return new Promise(function(resolve, reject)
-    {
-        let foundItem = findItem(dClient.eso.items, item);
+    let items = findItems(dClient.eso.items, item).map(i => deduceItemSummary(i));
+    let numberOfItems = items.length;
 
-        if (foundItem[0]) resolve(deduceItemSummary(foundItem));
-        else reject(`Item '${item}' not found`);
-    });
+    let randomItems = [];
+    if (items.length > 10)
+    {
+        for (let i = 0; i < 10; i++) randomItems.push(items.splice(Math.floor(Math.random() * items.length), 1)[0]);
+        return [ randomItems, numberOfItems ];
+    } else return [ items, numberOfItems ];
 }
 
-function esoItem(msg, item = null)
+function esoItem(msg, itemName = null)
 {
     if (msg.command && !msg.guild.config.guild.commands.esoItem.usage.command) return;
 
-    if (msg.command) item = msg.args.join(" ");
-    if (!item || !item.length) return;
+    if (msg.command) itemName = msg.args.join(" ");
+    if (!itemName || !itemName.length) return;
 
-    searchUESPItem(item)
-    .then(([itemObj, otherItems]) => {
-        console.log(JSON.stringify(itemObj, null, 2));
-        msg.channel.send((otherItems > 1) ? `There are **${otherItems - 1}** other items like this one` : "This item is quite unique, some say.", { files: [{ "name": `${itemObj.name}.png`, "attachment": itemObj.url }] }).catch(console.error);
-    }).catch(console.log);
+    try
+    {
+        let applicableItems, numberOfOtherItems;
+        [ applicableItems, numberOfOtherItems ] = searchUESPItems(itemName);
+
+        if (!applicableItems.length) msg.channel.send(`${msg.author.toString()} There are no items with the name \`${itemName}\`, it seems.`).catch(console.error);
+        else if (applicableItems.length === 1) msg.channel.send({ files: [{ "name": `${applicableItems[0].name}.png`, "attachment": applicableItems[0].url }] }).catch(console.error);
+        else
+        {
+            // collect user input for first random 10 items
+            let embedItems = applicableItems.map(item => { return { name: item.name, value: (item.description.length) ? item.description : "No description." } });
+            let collectorEmbed = new Discord.MessageEmbed(utils.createEmptyRichEmbedObject())
+                .setDescription(`There are a total of **${numberOfOtherItems}** items that match \`${itemName}\``)
+                .setFooter(`${constants.discord.embed.footer.text} | ${utils.fancyESODate(msg.createdAt)}`, constants.discord.embed.footer.icon_url);
+            collectorEmbed.fieldValueLength = 50;
+
+            utils.createSelectorEmbed(msg, embedItems, collectorEmbed).then(function([itemNumber, collectorMessage])
+            {
+                let selectedItem = applicableItems[itemNumber - 1];
+
+                let itemEmbed = new Discord.MessageEmbed(utils.createEmptyRichEmbedObject())
+                    .setColor(utils.randColor())
+                    .setAuthor(selectedItem.name, "", baseSummaryURL + selectedItem.id)
+                    .setDescription(selectedItem.description)
+                    .setImage(selectedItem.url)
+                    .setFooter(`${constants.discord.embed.footer.text} | ${utils.fancyESODate(msg.createdAt)}`, constants.discord.embed.footer.icon_url);
+
+                collectorMessage.edit({ embed: itemEmbed }).catch(console.error);
+            }).catch(console.error);
+        }
+    } catch (err)
+    {
+        console.error(err);
+        msg.channel.send(":x: Something went wrong while looking for this item. If this keeps happening, talk to a developer over at <https://bot.grogsile.org/discord>.").catch(console.error);
+    }
 }
 
 module.exports = esoItem;
