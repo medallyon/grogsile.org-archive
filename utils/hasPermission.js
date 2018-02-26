caf2080b5f07479e95a4205a6193c707fa810807
Tilman Raendchen <chopper98@hotmail.de>
2018-02-26 17:28:15 +0000

    Move contents of `bot` and `utils` to `lib`

--- utils/hasPermission.js
+++
@@ -1,9 +0,0 @@
-// boolean | returns whether the passed member has access to passed command
-var hasPermission = function(command, member)
-{
-    // return true if the member's permissions are higher than what is required by the command
-    return (utils.determinePermissions(member) >= command.userPermissions);
-}
-
-// export the hasPermission function
-module.exports = hasPermission;
